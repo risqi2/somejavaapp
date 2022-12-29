05ca9ef60ddedb2a0152007309296152d7a30266
risqi2 <risqiikhsani12@gmail.com>
2022-12-29 09:02:47 +0700

    test

---
+++ src/main/java/com/example/somejavaapp/SomejavaappApplication.java
@@ -0,0 +1,13 @@
+package com.example.somejavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomejavaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomejavaappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/somejavaapp/SomejavaappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.somejavaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomejavaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
